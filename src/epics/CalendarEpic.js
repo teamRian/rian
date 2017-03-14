36949f645a17263e9e68ab98e02794ec1cd75ca2
Duck Yeon Kim <kduckyeon@gmail.com>
2017-03-14 20:21:48 +0900

    remove error

--- src/epics/CalendarEpic.js
+++ src/epics/CalendarEpic.js
@@ -1,13 +1,49 @@
-// import { 
-// 	CALENDAR_GET_DATA, CALENDAR_REQUEST_DATA, CALENDAR_FAIL_DATA,
-// 	CALENDAR_POST_SEND, CALENDAR_POST_SUCCESS,CALENDAR_POST_FAIL,
-// 	CALENDAR_CHANGE_DATE, CALENDAR_SELECT_DATE, CALENDAR_TOGGLE   } from '../constants';
-// const CALE
-// const calendarPostSend = () => ({type:CALENDAR_POST_SEND});
-// const calendarPostSuccess = () => ({type:CALENDAR_POST_SUCCESS});
-// const calendarPostFail = () => ({type:CALENDAR_POST_FAIL});
+import Rx from 'rxjs/Rx';
+import firebase from 'firebase';
+import React from 'react';
+import { 
+	CALENDAR_EPIC_REQUEST_DATA,
+	CALENDAR_EPIC_FAIL_DATA,
+	CALENDAR_EPIC_SUCCESS_DATA,
+	CALENDAR_EPIC_CANCLE_DATA 
+} from '../constants'
 
-// const calendarPostEpic = (action$, store) =>
-// 	action$.ofType(calendarPostSend)
-// 		.filter()=> console.log(store.getState())
+export const CalendarEpic = (action$, store, getFirebase) => {
+	return action$.ofType(CALENDAR_EPIC_REQUEST_DATA)
+		.switchMap(action=>{
+			return Rx.Observable.fromPromise(getFirebase().ref(`/duck/${store.getState().User._id}/plans`).on('value'))
+				.map(snapshot => { 	
+					return calendarEpicSuccessData(snapshot.val()) 
+				})
+				.takeUntil(action$.ofType(CALENDAR_EPIC_CANCLE_DATA))
+				.catch(err => calendarEpicFailData(err))
+		})
+}
 
+
+export function calendarEpicRequestData(sorting){
+	return {
+		type: CALENDAR_EPIC_REQUEST_DATA,
+		howSorting: sorting
+	}
+}
+
+export function calendarEpicSuccessData(response, b){
+	return {
+		type: CALENDAR_EPIC_SUCCESS_DATA,
+		data: response,
+		howSorting: b
+	}
+}
+
+export function calendarEpicCancleData(response){
+	return {
+		type: CALENDAR_EPIC_CANCLE_DATA
+	}
+}
+
+export function calendarEpicFailData(err){
+	return {
+		type: CALENDAR_EPIC_FAIL_DATA
+	}
+}
--- src/epics/index.js
+++ src/epics/index.js
@@ -2,12 +2,14 @@ import 'rxjs';
 import { combineEpics } from 'redux-observable';
 import { getFirebase } from 'react-redux-firebase'
 import { NoteEpic, NoteOneEpic } from './NoteEpic';
+import { CalendarEpic } from './CalendarEpic';
 // import { ChatEpic } from './ChatEpic';
 
 const rootEpic = (... args) => {
 	return combineEpics(
 		NoteEpic,
 		NoteOneEpic,
+		CalendarEpic,
 	)(...args, getFirebase);
 }
 
