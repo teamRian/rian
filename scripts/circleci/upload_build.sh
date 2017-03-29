1ba193fe10f6f236f44e54fc2a148a7770196dc1
Duck Yeon Kim <kduckyeon@gmail.com>
2017-03-29 16:45:55 +0900

    install aws

--- scripts/circleci/upload_build.sh
+++ scripts/circleci/upload_build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # if [ "${CIRCLE_BRANCH}" == "master" ]; then
+	sudo apt-get install -y python-pip
+	sudo pip install awscli
 	aws s3 sync ~/rian s3://rian-s3-dev/ --delete
 # fi
