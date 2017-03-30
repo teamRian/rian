d066f87390ff3e9f740349fadc80439d081deca1
Wuyialex <cci45@naver.com>
2017-03-30 12:30:24 +0900

    readytoMerge

--- src/containers/FirebaseChat/FirebaseChatContainer.js
+++ src/containers/FirebaseChat/FirebaseChatContainer.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes } from "react";
 import { connect } from "react-redux";
 import "./css/style.css";
 import { graphql, compose } from "react-apollo";
-
 import gql from "graphql-tag";
 
 // import {
