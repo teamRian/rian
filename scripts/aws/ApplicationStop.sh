8d7ab9c7df159dd33db6c0772ea4e308fac3739a
Duck Yeon Kim <kduckyeon@gmail.com>
2017-04-01 16:33:28 +0900

    try

--- scripts/aws/ApplicationStop.sh
+++ scripts/aws/ApplicationStop.sh
@@ -6,10 +6,12 @@ if [ -f "$file" ]
 then
 	echo "$file found, close docker and remove existing files"
 	sudo docker-compose down
-	sudo rm -rf ~/rian/*
+	sudo rm -rf ~/rian
+	mkdir -p ~/rian
 else
 	echo "$file not found, continue download bundle"
-	sudo rm -rf ~/rian/*
+	sudo rm -rf ~/rian
+	mkdir -p ~/rian
 fi
 
 
