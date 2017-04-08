ca7eea92aae9071e780fb3b0dc64a9afb7fa7ab3
Duck Yeon Kim <kduckyeon@gmail.com>
2017-04-08 19:57:48 +0900

    improved auth flow

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,15 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 const Project = new Schema({
-	name: { type: String, required: true },
-	creator : { type: Schema.Types.ObjectId, ref: 'Users'},
-	member: [{ type: Schema.Types.ObjectId, ref: "Users"}], 
-	chatroom: { type: Schema.Types.ObjectId, ref: "Chatrooms" },
-	whiteboard: [{ type: Schema.Types.ObjectId, ref: "Whiteboards" }],
-	created_at: { type: 'Date', default: Date.now, required: true }
+  name: { type: String, required: true },
+  creator: { type: Schema.Types.ObjectId, ref: "Users" },
+  member: [{ type: Schema.Types.ObjectId, ref: "Users" }],
+  chatroom: { type: Schema.Types.ObjectId, ref: "Chatrooms" },
+  whiteboard: [{ type: Schema.Types.ObjectId, ref: "Whiteboards" }],
+  created_at: { type: "Date", default: Date.now, required: true }
 });
 
-export default mongoose.model('Projects', Project);
-
+export default mongoose.model("Projects", Project);
--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -1,3 +1,11 @@
+import { 
+	USER_REQUEST_CHECK_AUTH,
+	USER_REQUEST_LOG_IN, USER_SUCCESS_LOG_IN, USER_FAIL_LOG_IN,
+  USER_REQUEST_SIGN_UP, USER_SUCCESS_SIGN_UP, USER_FAIL_SIGN_UP,
+  USER_LOG_OUT, 
+  USER_REQUEST_ADD_PROJECT, USER_SUCCESS_ADD_PROJECT, USER_FAIL_ADD_PROJECT
+} from '../constants';
+
 var UserState = {
 	_id: null,
 	email: null,
@@ -5,20 +13,21 @@ var UserState = {
 	picture: null,
 	token: null,
 	loading: false,
-	facebook_id: null
+	facebook_id: null,
+	projects: []
 }
 
 export function User(state = UserState, action) {
 	switch (action.type){
-		case "USER_REQUEST_CHECK_AUTH":
+		case USER_REQUEST_CHECK_AUTH:
 			return Object.assign({}, state, {
 				loading: action.loading
 			})
-		case "USER_REQUEST_LOG_IN":
+		case USER_REQUEST_LOG_IN:
 			return Object.assign({}, state, {
 				loading: action.loading
 			})
-		case "USER_SUCCESS_LOG_IN":
+		case USER_SUCCESS_LOG_IN:
 			return Object.assign({}, state, {
 				loading: action.loading,
 				_id: action._id,
@@ -26,18 +35,19 @@ export function User(state = UserState, action) {
 				name: action.name,
 				facebook_id: action.facebook_id,
 				token: action.token,
-			  picture: action.picture
+			  picture: action.picture,
+			  projects: action.projects
 			})
-		case "USER_FAIL_LOG_IN":
+		case USER_FAIL_LOG_IN:
 			return Object.assign({}, state, {
 				loading: action.loading
 			})
 
-		case "USER_REQUEST_SIGN_UP":
+		case USER_REQUEST_SIGN_UP:
 			return Object.assign({}, state, {
 				loading: action.loading
 			})
-		case "USER_SUCCESS_SIGN_UP":
+		case USER_SUCCESS_SIGN_UP:
 			return Object.assign({}, state, {
 				loading: action.loading,
 				_id: action._id
@@ -45,11 +55,11 @@ export function User(state = UserState, action) {
 				// profilePhoto: action.profilePhoto,
 				// token: action.token
 			})
-		case "USER_FAIL_SIGN_UP":
+		case USER_FAIL_SIGN_UP:
 			return Object.assign({}, state, {
 				loading: action.loading
 			})
-		case "USER_LOG_OUT":
+		case USER_LOG_OUT:
 			return Object.assign({}, state, {
 				loading: action.loading,
 				_id: action._id,
@@ -57,7 +67,21 @@ export function User(state = UserState, action) {
 				name: action.name,
 				facebook_id: action.facebook_id,
 				token: action.token,
-			  picture: action.picture
+			  picture: action.picture,
+			  projects: action.projects
+			})
+		case USER_REQUEST_ADD_PROJECT:
+			return Object.assign({}, state, {
+				loading: action.loading
+			})
+		case USER_SUCCESS_ADD_PROJECT:
+			return Object.assign({}, state, {
+				loading: action.loading,
+				projects: [...state.projects, action.projects]
+			})
+		case USER_FAIL_ADD_PROJECT:
+			return Object.assign({}, state, {
+				loading: action.loading
 			})
 		default:
 			return state
